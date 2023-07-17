a0e82e96fd509f1e8a8289cbc664334fb9af8a67
ayang007 <86279159+ayang007@users.noreply.github.com>
2023-07-16 20:34:38 -0700

    attempt to squash

--- main.js
+++ main.js
@@ -1,9 +1,16 @@
 import './style.css'
 import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+import {GLTFLoader} from 'three/examples/jsm/loaders/GLTFLoader';
 
 const scene = new THREE.Scene()
 
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+//models
+const islandUrl = new URL('./assets/island.glb', import.meta.url)
+const treehouseUrl = new URL('./assets/treehouse.glb', import.meta.url)
+
 
 const renderer = new THREE.WebGL1Renderer({
     canvas: document.querySelector('#bg'),
@@ -21,6 +28,24 @@ const torus = new THREE.Mesh(geometry, material);
 
 scene.add(torus)
 
+const assetLoader = new GLTFLoader();
+
+//loading assets
+assetLoader.load(islandUrl.href, function(gltf) {
+    const model = gltf.scene;
+    scene.add(model);
+    model.position.set(0, 0, 0);
+}, undefined, function(error) {
+    console.error(error);
+});
+
+assetLoader.load(treehouseUrl.href, function(gltf) {
+    const model = gltf.scene;
+    scene.add(model);
+    model.position.set(0, 0, 0);
+}, undefined, function(error) {
+    console.error(error);
+});
 
 //lights
 const sun = new THREE.PointLight(0xffffff)
@@ -30,15 +55,21 @@ const ambientLight = new THREE.AmbientLight(0xffffff);
 
 scene.add(sun, ambientLight)
 
-//light helper
+//helper
 const lightHelper = new THREE.PointLightHelper(sun)
-scene.add(lightHelper)
+const gridHelper = new THREE.GridHelper(200, 50);
+
+scene.add(lightHelper, gridHelper)
+
+const controls = new OrbitControls(camera, renderer.domElement);
 
 function animate() {
     requestAnimationFrame( animate );
     torus.rotation.x += 0.01;
     torus.rotation.y += 0.005;
     torus.rotation.z += 0.01;
+
+    controls.update();
 
     renderer.render( scene, camera);
 }
