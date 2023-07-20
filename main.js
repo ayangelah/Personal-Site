1724bac68408ce0fa879c0739409f78c3f18da7a
ayang007 <86279159+ayang007@users.noreply.github.com>
2023-07-20 03:17:54 -0700

    finished rough three js website with redirects, next steps include toggle off redirects, html elements, aesthetic changes

--- main.js
+++ main.js
@@ -35,7 +35,14 @@ scene.add(skybox);
 //models
 const islandUrl = new URL('./assets/island.glb', import.meta.url)
 const rocksUrl = new URL('./assets/rocks.glb', import.meta.url)
+const cloudsUrl = new URL('./assets/clouds.glb', import.meta.url)
 const treehouseUrl = new URL('./assets/treehouse.glb', import.meta.url)
+const researchUrl = new URL('./assets/research.glb', import.meta.url)
+const artUrl = new URL('./assets/art.glb', import.meta.url)
+const socialsUrl = new URL('./assets/socials.glb', import.meta.url)
+const mailboxUrl = new URL('./assets/mailbox.glb', import.meta.url)
+const lightbulbUrl = new URL('./assets/lightbulb.glb', import.meta.url)
+const gamedevUrl = new URL('./assets/gamedev.glb', import.meta.url)
 const whaleUrl = new URL('./assets/whale.glb', import.meta.url)
 const airplaneUrl = new URL('./assets/airplane.glb', import.meta.url)
 
@@ -45,7 +52,7 @@ const renderer = new THREE.WebGLRenderer({
 
 renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(window.innerWidth, window.innerHeight);
-camera.position.setZ(30);
+camera.position.setZ(15);
 
 renderer.render( scene, camera);
 
@@ -59,15 +66,14 @@ window.addEventListener('click', onClick);
 const assetLoader = new GLTFLoader();
 
 //loading assets
-var objectlist = [];
+
+//island and decor
 
 var island = new THREE.Object3D;
 assetLoader.load(islandUrl.href, function(gltf) {
     island = gltf.scene;
     scene.add(island);
     island.name = "island";
-    island.userData.clickable = false;
-    island.userData.name = "island";
     island.position.set(0, 0, 0);
 }, undefined, function(error) {
     console.error(error);
@@ -77,8 +83,6 @@ var rocks = new THREE.Object3D;
 assetLoader.load(rocksUrl.href, function(gltf) {
     rocks = gltf.scene;
     scene.add(rocks);
-    rocks.userData.clickable = false;
-    rocks.userData.name = "rocks";
     rocks.name = "rocks";
     rocks.position.set(0, 0, 0);
     gsap.to( rocks.position, {
@@ -92,26 +96,88 @@ assetLoader.load(rocksUrl.href, function(gltf) {
     console.error(error);
 });
 
+var clouds = new THREE.Object3D;
+assetLoader.load(cloudsUrl.href, function(gltf) {
+    clouds = gltf.scene;
+    scene.add(clouds);
+    clouds.name = "clouds";
+    clouds.position.set(0, 0, 0);
+}, undefined, function(error) {
+    console.error(error);
+});
+
+//resumetreehouse
 var treehouse = new THREE.Object3D;
 assetLoader.load(treehouseUrl.href, function(gltf) {
     treehouse = gltf.scene;
     scene.add(treehouse);
-    treehouse.userData.clickable = true;
-    treehouse.userData.name = "treehouse";
     treehouse.name = "treehouse";
 }, undefined, function(error) {
     console.error(error);
 });
 
+//six options
+var research = new THREE.Object3D;
+assetLoader.load(researchUrl.href, function(gltf) {
+    research = gltf.scene;
+    scene.add(research);
+    research.name = "research";
+}, undefined, function(error) {
+    console.error(error);
+});
+
+var art = new THREE.Object3D;
+assetLoader.load(artUrl.href, function(gltf) {
+    art = gltf.scene;
+    scene.add(art);
+    art.name = "art";
+}, undefined, function(error) {
+    console.error(error);
+});
+
+var socials = new THREE.Object3D;
+assetLoader.load(socialsUrl.href, function(gltf) {
+    socials = gltf.scene;
+    scene.add(socials);
+    socials.name = "socials";
+}, undefined, function(error) {
+    console.error(error);
+});
+
+var mailbox = new THREE.Object3D;
+assetLoader.load(mailboxUrl.href, function(gltf) {
+    mailbox = gltf.scene;
+    scene.add(mailbox);
+    mailbox.name = "mailbox";
+}, undefined, function(error) {
+    console.error(error);
+});
+
+var lightbulb = new THREE.Object3D;
+assetLoader.load(lightbulbUrl.href, function(gltf) {
+    lightbulb = gltf.scene;
+    scene.add(lightbulb);
+    lightbulb.name = "lightbulb";
+}, undefined, function(error) {
+    console.error(error);
+});
+
+var gamedev = new THREE.Object3D;
+assetLoader.load(gamedevUrl.href, function(gltf) {
+    gamedev = gltf.scene;
+    scene.add(gamedev);
+    gamedev.name = "gamedev";
+}, undefined, function(error) {
+    console.error(error);
+});
+
+//extras
 let whalemixer;
 var whale = new THREE.Object3D;
 assetLoader.load(whaleUrl.href, function(gltf) {
     whale = gltf.scene;
     scene.add(whale);
-    whale.userData.clickable = true;
-    whale.userData.name = "whale";
     whale.name = "whale";
-    //objectlist.push(whale);
     whale.position.set(0, 0, 0);
     whalemixer = new THREE.AnimationMixer(whale);
     const clips = gltf.animations;
@@ -127,9 +193,6 @@ var airplane;
 assetLoader.load(airplaneUrl.href, function(gltf) {
     airplane = gltf.scene;
     scene.add(airplane);
-    //objectlist.push(airplane);
-    airplane.userData.clickable = true;
-    airplane.userData.name = "airplane";
     airplane.name = "airplane";
     airplane.position.set(0, 0, 0);
 }, undefined, function(error) {
@@ -156,11 +219,80 @@ function onClick(event) {
 
     if (intersects.length > 1 && intersects[0].object.name != undefined) {
         let selected = intersects[0].object.name;
-        alert(selected);
+        //alert(selected);
         //switch statement with links
         switch(selected) {
+            //resume
+            case "Plane011":
+            case "Plane011_1":
+            case "Plane011_2":
+            case "Plane011_3":
+            case "Plane011_4":
+            case "Plane011_5":
+            case "Plane011_6":
+            case "Plane011_7":
+            case "Plane011_8":
+            case "Plane011_9":
+            case "Plane011_10":
+            case "Plane011_11":
+            case "Plane011_12":
+            case "Plane011_13":
+            case "Plane011_14":
+                window.open("http://ayangelah.me/attachments/Resume%207_7_23.pdf");
+                break;
+            //1: research
+            case "Cube015":
+                window.open("http://ayangelah.me/attachments/The Effects of Citric Acid on Contraction of Mouse Cardiomyocytes.pdf");
+                break;
+            case "Cube017":
+                window.open("http://ayangelah.me/attachments/Illustrations of Ukrainian Phonetics.pdf");
+                break;
+            case "Cube018":
+                window.open("http://ayangelah.me/attachments/Tone Sandhi in Hokkien Taiwanese.pdf");
+                break;
+            case "Bookshelf":
+            case "Label":
+                window.open("http://ayangelah.me/research.html");
+                break;
+            //2: art
+            case "Cube014":
+            case "Cube014_1":
+            case "Cube014_2":
+            case "Cube014_3":
+            case "Cube014_4":
+            case "Cube014_5":
+            case "Cube014_6":
+                window.open("https://www.artpal.com/jackelope");
+                break;
+            //3: socials
+            case "Cube008":
+            case "Cube008_1":
+                window.open("https://www.linkedin.com/in/ayangelah");
+                break;
+            case "Cube009":
+            case "Cube009_1":
+                window.open("https://www.github.com/ayangelah");
+                break;
+            case "Cube012":
+            case "Cube012_1":
+                //discord stuff
+                break;
+            //4: contact me
+            case "Mailbox":
+                window.open("mailto:ayangelah@gmail.com");
+            //5: devpost/hackathons
+            case "Torus001":
+            case "Torus001_1":
+                window.open("https://devpost.com/ayang007?ref_content=user-portfolio&ref_feature=portfolio&ref_medium=global-nav");
+                break;
+            //6: gamedev
+
+            //extras
             case "Whale": 
                 window.open("https://open.spotify.com/user/32yl40xhp98r8uziid81m33xo?si=86f01a2452a14632");
+                break;
+            case "Airplane":
+                window.open("http://ayangelah.me/blog.html");
                 break;
 
         }
@@ -221,6 +353,7 @@ controls.maxDistance = 100;
 controls.minDistance = 6;
 controls.enableDamping = true;
 controls.enablePan = false;
+controls.autoRotate = true;
 
 const clock = new THREE.Clock();
 function animate() {
@@ -233,6 +366,9 @@ function animate() {
     }
     if (airplane) {
         airplane.rotateY(-0.007);
+    }
+    if (clouds) {
+        clouds.rotateY(0.008);
     }
     controls.update();
     //clickMesh();
